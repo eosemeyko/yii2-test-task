ded77b9985ed94364eaf4326d44d06327e679862
Egor Semeiko <raziel2283@mail.ru>
2018-03-07 03:41:40 +0300

    Removed excess text on the authorization page. Added title and breadcrumb in sign up page.

--- views/auth/sign-up.php
+++ views/auth/sign-up.php
@@ -1,14 +1,19 @@
 <?php
 
+/* @var $this yii\web\View */
+/* @var $model app\models\SignUpForm */
+/* @var $form yii\bootstrap\ActiveForm */
+
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\widgets\Pjax;
 
-/* @var $model app\models\SignUpForm */
-/* @var $form ActiveForm */
+$this->title = 'Sign up';
+$this->params['breadcrumbs'][] = $this->title;
 ?>
 <?php Pjax::begin(); ?>
 <div class="sign-up">
+    <h1><?= Html::encode($this->title) ?></h1>
 
     <?php if ($registration_success): ?>
         <div class="alert alert-success"> Регистрация успешно выполнена! </div>
